cd8009db285d4346173aae42a079ae3640aaed3a
Abi L <124858748+lailiel@users.noreply.github.com>
2023-07-16 21:18:59 -0500

    built out routes, created controller files

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {
+  getThought,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+} = require('../../controllers/thoughtController.js');
+
+// /api/courses
+router.route('/').get(getThought).post(createThought);
+
+// /api/courses/:courseId
+router
+  .route('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {
+  getUser,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require('../../controllers/userController.js');
+
+// /api/user
+router.route('/').get(getUser).post(createUser);
+
+// /api/user/:userId
+router
+  .route('/:userId')
+  .get(getSingleUser)
+  .put(updateUser)
+  .delete(deleteUser);
+
+module.exports = router;
